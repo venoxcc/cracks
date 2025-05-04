5a85e8d21ba51a80b4adb381ae00d227e32ad541
venox <167776494+venoxcc@users.noreply.github.com>
2025-05-04 20:25:08 +0200

    Create loader.lua

---
+++ venoxscripts-repo/content/sources/loader.lua
@@ -0,0 +1,8 @@
+--predefined skin changes
+getgenv().swap = {
+    ["Assault Rifle"] = "AKEY-47",
+    ["Sniper"] = "Eyething Sniper",
+}
+
+getgenv().AutoLoadDisabled=false
+loadstring(game:HttpGet('https://raw.githubusercontent.com/venoxcc/universalscripts/refs/heads/main/rivals/venoxware'))()
