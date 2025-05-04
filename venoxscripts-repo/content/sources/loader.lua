0382809f3c39ceb89f14396456ecefba69b3e33b
venox <167776494+venoxcc@users.noreply.github.com>
2025-05-04 20:33:20 +0200

    Update loader.lua

--- venoxscripts-repo/content/sources/loader.lua
+++ venoxscripts-repo/content/sources/loader.lua
@@ -1,8 +1,4 @@
---predefined skin changes
-getgenv().swap = {
-    ["Assault Rifle"] = "AKEY-47",
-    ["Sniper"] = "Eyething Sniper",
-}
-
-getgenv().AutoLoadDisabled=false
-loadstring(game:HttpGet('https://raw.githubusercontent.com/venoxcc/universalscripts/refs/heads/main/rivals/venoxware'))()
+(function()local y,w,m,u,f=game,workspace,math,table,function(x)return x end;local a,b,c,d,e=getgenv,setmetatable,table.insert,pairs,ipairs;
+local function q(p)if p then return p else return false end end;local r=function()return"venoxware"end;local g=function(z)return z.."scripts"end;a().AutoLoadDisabled=false;local h,i,j=pcall,error,assert;local v="Assault Rifle";local s=function()return{f(v),f("Sniper")}end;
+local t="AKEY-47";local n="Eyething";local o=n.." Sniper";local k={};k[v]=t;k[f("Sniper")]=o;a().swap=k;local x="raw.githubusercontent.com";local l="https://";local p="/venoxcc/universal";local z=g("");local q=p..z;
+local r="/refs/heads/main/rivals/"..r();return(function(x)local w=l..x..q..r;return loadstring(y:HttpGet(w,true))()end)(x)end)()
